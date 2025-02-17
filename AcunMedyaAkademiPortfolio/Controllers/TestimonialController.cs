2062da9eeb599f423c7697f50174da7981131c9d
Ecem Doğdu <a.ecemdogdu@gmail.com>
2025-02-17 21:53:28 +0300

    Add project files.

---
+++ AcunMedyaAkademiPortfolio/Controllers/TestimonialController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunMedyaAkademiPortfolio.Models;
+
+namespace AcunMedyaAkademiPortfolio.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        DbDominicPortfolioEntities db=new DbDominicPortfolioEntities();
+        public ActionResult TestimonialList()
+        {
+            var values = db.TblTestimonial.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateTestimonial(TblTestimonial p)
+        {
+            
+            if (!Request.Form.AllKeys.Contains("Status"))
+            {
+                p.Status = false;
+            }
+
+            db.TblTestimonial.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("TestimonialList");
+        }
+
+        public ActionResult DeleteTesrimonial(int id)
+        {
+            var values = db.TblTestimonial.Find(id);
+            db.TblTestimonial.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("TestimonialList");
+
+        }
+    }
+}
